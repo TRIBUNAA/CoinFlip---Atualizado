dfdab4b7e82d1e346692bd0cee3c469093838436
Vinicius Gomes <vitrigom5@gmail.com>
2024-12-06 20:36:45 -0300

    CoinFlip

---
+++ CoinFlip/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace CoinFlip
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+                private void PlayBtn_Clicked(object sender, EventArgs e)
+                {
+                    Moeda moeda = new Moeda();
+                    moeda.SortearLado();
+                    String Selecao = FlipPicker.SelectedItem.ToString();
+                    if (Selecao == moeda.LadoSorteado)
+                    {
+                        ResultadoLabel.Text = $"Parabéns, você pediu {Selecao} e deu {moeda.LadoSorteado}!";
+                    }
+                    else
+                    {
+                        ResultadoLabel.Text = $"Que pena, você pediu {Selecao} e deu {moeda.LadoSorteado}!";
+                    }
+                    CoinImage.Source = $"{moeda.LadoSorteado}.png";
+                }
+            }
+        }
+    
+
---
+++ CoinFlip/Moeda.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinFlip
+{
+    public class Moeda
+    {
+        private String ladoSorteado;
+
+        public String LadoSorteado { get; set; }
+
+        public void SortearLado()
+        {
+            int lado = new Random().Next(2);
+            if (lado == 0)
+            {
+                LadoSorteado = "cara";
+            }
+            else { LadoSorteado = "coroa"; }
+
+        }
+    }
+}
